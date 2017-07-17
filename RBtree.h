8cd1eedd2c4f0c611f4a398ebcf78be25c3fd4d3
xidulu <xidulu@gmail.com>
2017-07-17 17:11:05 +0800

    Insertion&&print

--- RBtree.h
+++ RBtree.h
@@ -1,12 +1,13 @@
-#ifndef RBTREE_H
-#define RBTREE_H
-
+#ifndef RBTREE_H_INCLUDED
+#define RBTREE_H_INCLUDED
+#include <iostream>
+using namespace std;
 
 class Node{
 private:
 	friend class RBtree;
 	int key;
-	Node *parent,*left,*right;
+	Node *p,*left,*right;
 	int size;
 	bool color;
 public:
@@ -14,23 +15,45 @@ public:
 		key=x;
 		size=1;
 	}
-}
+};
 
 class RBtree{
 private:
 	void LeftRotate(Node* );
 	void RightRotate(Node* );
-	void InsertFix(Node*)
+	void InsertFix(Node* &);
 	int NUM;
 	Node *Root,*Nil;
 	static bool RED;
 	static bool BLACK;
 public:
 	void Insert(int);
-}
+	RBtree(){
+	    cout<<"FUCK"<<endl;
+		Nil=new Node(-1);
+		Nil->color=BLACK;
+		Nil->size=0;
+		Root=Nil;
+		NUM=0;
+	}
+	void print();
+	void print(Node* t);
+};
 
 bool RBtree::BLACK=false;
 bool RBtree::RED=true;
+
+void RBtree::print()
+{
+	print(Root);
+}
+
+void RBtree::print(Node* x)
+{
+    if(x->left!=Nil) print(x->left);
+    for(int i=0;i<x->size;i++) cout<<x->key<<' ';
+    if(x->right!=Nil) print(x->right);
+}
 
 void RBtree::LeftRotate(Node* x)
 {
@@ -63,7 +86,7 @@ void RBtree::RightRotate(Node *x)
 {
 //  ((a,y,b),x,c)
 //  RightRotate
-//  (a,y,(b,x,c)) 
+//  (a,y,(b,x,c))
 	Node* y=x->left;
 //  link b to x
 	x->left=y->right;
@@ -78,7 +101,7 @@ void RBtree::RightRotate(Node *x)
 		if(x==x->p->left){
 			x->p->left=y;
 		}else{
-			x->p->rigth=y;
+			x->p->right=y;
 		}
 	}
 //  link x to its new father:y
@@ -87,7 +110,7 @@ void RBtree::RightRotate(Node *x)
 }
 
 void RBtree::Insert(int t)
-{		
+{
 	NUM++;
 	Node* y=Nil;
 	Node* x=Root;
@@ -123,18 +146,55 @@ void RBtree::Insert(int t)
 }
 
 /*
-({B},({R},({R},A,x,B),y,C),z,D) 
-({B},A,x,({R},B,y,({R},C,z,D))) 
-({B},({R},A,x,({R},B,y,C)),z,D) 
-({B},A,z,({R},({R},B,y,C),z,D)) 
-balcance-> 
-({R},({B},A,x,B),y,({B},C,z,D)) 
-*/  
+Reference:
+
+http://alrightchiu.github.io/SecondRound/red-black-tree-insertxin-zeng-zi-liao-yu-fixupxiu-zheng.html
 
 
-void RBtree::InsertFix(Node *x)
+*/
+
+
+
+void RBtree::InsertFix(Node * &z)
 {
-	 
+	while(z->p->color==RED){
+		if (z->p==z->p->p->left){
+			Node* y=z->p->p->right;
+			if(y->color==RED){
+				z->p->color=BLACK;
+				y->color=BLACK;
+				z->p->p->color=RED;
+				z=z->p->p;
+			}else{
+				if (z==z->p->right){
+					z=z->p;
+					LeftRotate(z);
+				}
+				z->p->color=BLACK;
+				z->p->p->color=RED;
+				RightRotate(z->p->p);
+			}
+		}else{
+			Node* y=z->p->p->left;
+			if(y->color==RED){
+				z->p->color = BLACK;
+				y->color = BLACK;
+				z->p->p->color = RED;
+				z = z->p->p;
+			}else{
+				if (z==z->p->left){
+					z=z->p;
+					RightRotate(z);
+				}
+				z->p->color=BLACK;
+				z->p->p->color=RED;
+				LeftRotate(z->p->p);
+			}
+		}
+	}
+	Root->color=BLACK;
 }
 
-#endif
+
+
+#endif // RBTREE_H_INCLUDED
